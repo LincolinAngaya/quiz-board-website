0dbf0794ce11c56b3fd251a075c7da876f2a1c45
lincolinAngaya <lincolinangaya5626@gmail.com>
2022-05-14 18:49:15 +0300

    javascript program show the total score

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,17 @@
-function output(){
-  document.write("hello world");
+function output()
+{
+  var i=0;
+  var q1=document.quiz.quiz1.value;
+  var q2=document.quiz.quiz2.value;
+  var q3=document.quiz.quiz3.value;
+  var q4=document.quiz.quiz4.value;
+  var q5=document.quiz.quiz5.value;
+
+  if(q1=="All of the Above") {i++}
+  if(q2=="Base Object") {i++}
+  if(q3=="//") {i++}
+  if(q4=="It is used to spread iterables to individual elements") {i++}
+  if(q5=="learInterval") {i++}
+
+  document.write(i);
 }
