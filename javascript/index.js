4f479603d49528a880ca4a829425feab262a3086
lincolinAngaya <lincolinangaya5626@gmail.com>
2022-05-14 23:18:57 +0300

    delete the filepage4

--- javascript/index.js
+++ javascript/index.js
@@ -12,12 +12,13 @@ function output()
   var q3=document.quiz.quiz3.value;
   var q4=document.quiz.quiz4.value;
   var q5=document.quiz.quiz5.value;
-
+var result=document.getElementById("result");
+ 
   if(q1=="All of the Above") {i++}
   if(q2=="Base Object") {i++}
   if(q3=="//") {i++}
   if(q4=="It is used to spread iterables to individual elements") {i++}
   if(q5=="learInterval") {i++}
 
-  document.write(i);
+  result.testContent='${c}';
 }
